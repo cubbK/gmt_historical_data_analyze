2b90badc4dceb179926042e6fee0bb7afbd8a5c0
Dan <yo@gmail.om>
2025-03-21 14:21:31 +0100

    scrap links

--- scrap.py
+++ scrap.py
@@ -7,18 +7,44 @@ with sync_playwright() as p:
         "https://web.archive.org/web/*/https://www.gmtgames.com/m-2-gmt-games.aspx"
     )
 
-    year_elements = page.locator("#year-labels > sparkline-year-label")
+    # Locate all year elements
+    locator = page.locator("#year-labels > .sparkline-year-label")
+    locator.first.wait_for(state="attached", timeout=10000)  # Wait for at least one
+    year_elements = locator.all()
+
     valid_years = {"2020", "2021", "2022", "2023", "2024", "2025"}
 
-    # Collect valid year elements into a list
-    valid_years = []
-    for i in range(year_elements.count()):
-        year_text = year_elements.nth(i).inner_text().strip()
-        if year_text in valid_years:
-            valid_years.append(year_text)
-        else:
-            print(f"Excluding element with text: {year_text}")
+    # Filter year_elements where inner text is in valid_years
+    filtered_year_elements = [
+        element
+        for element in year_elements
+        if element.evaluate("el => el.innerText") in valid_years
+    ]
 
-    # Print the filtered list of valid years
-    print("Valid years:", valid_years)
-    print(page.title())
+    link_dictionary = {}
+
+    # Print the page title
+    for year_element in filtered_year_elements:
+        year_element.click()
+
+        locator = page.locator(".calendar-grid .calendar-day")
+        locator.first.wait_for(state="attached", timeout=10000)  # Wait for at least one
+        day_elements = locator.all()
+
+        for day_element in day_elements:
+            print("here")
+            day_element.hover(force=True)
+            popup_title = page.locator(".popup-of-day .day-tooltip-title").inner_text()
+
+            popup_link_link = page.locator(
+                ".popup-of-day ul li:last-child a"
+            ).get_attribute("href")
+
+            if popup_title:
+                link_dictionary[popup_title] = popup_link_link
+            else:
+                raise ValueError("No title found", day_element.inner_text())
+
+        print("yo")
+
+    browser.close()
